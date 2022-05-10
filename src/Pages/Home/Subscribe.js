46df51f2447d2a051bb6541be11a8585292bc9e1
HabibOnTheGo <HabibOnTheGo12@gmail.com>
2022-05-10 13:23:32 +0600

    done all Comment section

--- src/Pages/Home/Subscribe.js
+++ src/Pages/Home/Subscribe.js
@@ -3,41 +3,39 @@ import img from "../../image/image5.jpg";
 
 const Subscribe = () => {
   return (
-    <div className="flex justify-around items-center bg-gray-200">
-      <div className="bg-white p-10 m-20 rounded-lg text-left">
-        <h1 className="text-primary text-4xl pb-5">Tell Us Your Message</h1>
-        <div className="w-full mx-auto">
-          <input
-            type="text"
-            placeholder="Name"
-            class="input input-primary w-full max-w-xs mb-5"
-          />
-          <br />
-          <input
-            type="email"
-            placeholder="Email"
-            class="input input-primary w-full max-w-xs mb-5"
-          />{" "}
-          <br />
-          <input
-            type="text"
-            placeholder="Subject"
-            class="input input-primary w-full max-w-xs mb-5"
-          />
-          <br />
-          <textarea
-            class="textarea input input-primary w-full max-w-xs mb-5"
-            placeholder="Comment"
-          ></textarea>
-        </div>
-        <button class="btn btn-outline btn-primary">Send Massage</button>
+    <div class="hero min-h-screen bg-base-200">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+      <div class="text-center lg:text-left">
+        <h1 class="text-5xl font-bold">Please Comment</h1>
+        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
       </div>
-
-      <div>
-        <img className="rounded-lg" src={img} alt="" />
+      <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+        <div class="card-body">
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Email</span>
+            </label>
+            <input type="text" placeholder="email" class="input input-bordered" />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Password</span>
+            </label>
+            <input type="text" placeholder="password" class="input input-bordered" />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Comment</span>
+            </label>
+            <textarea type="text" placeholder="Comment" class="input input-bordered py-10" />
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn btn-primary">Send</button>
+          </div>
+        </div>
       </div>
     </div>
-  );
+  </div>)
 };
 
 export default Subscribe;
